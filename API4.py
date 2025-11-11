291623d751f46fcae57b778a534f8b81b63fb53c
64LONG <64298723+64LONG@users.noreply.github.com>
2025-11-11 17:23:19 -0600

    Python script using FastAPI with HTTP Methods and user input for blogs
    Python script to  test foundational skill sets using API with user input in regards to blogs. Starting with FastAPI with HTTP methods with user input to trigger each HTTP METHODS [GET, POST, PUT, DELETE].

---
+++ API4.py
@@ -0,0 +1,117 @@
+#Importing package Web API, FastAPI
+from fastapi import FastAPI, HTTPException
+#Importing pydantic package to classify and manage data
+from pydantic import BaseModel
+#Importing Typing package to note expected data for variables and functions
+from typing import List, Optional
+#Importing UUID package for unique identifers to use
+from uuid import uuid4, UUID
+#Importing UUID package for unique identifers to use
+import requests
+
+app= FastAPI()
+
+#Blog class defintiion with variables
+class Blog(BaseModel):
+    id: Optional[UUID] = uuid4()
+    title: str
+    content: str
+    tags: Optional[str] = None
+
+#In-memory Storage, an array
+blogs_db = List[Blog]
+
+
+#Option "test" --- Connection test for Sanity Test
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}    
+
+#Option #1 --- Creating blog and adding into array
+@app.post("/blogs", response_model=Blog)
+async def create_blog(blog:Blog):
+    blogs_db.append(blog)
+    return blog
+
+#Option #2 --- Read all blogs within the array
+@app.get("/blogs", response_model=List[Blog])
+async def read_blogs():
+    return blogs_db
+
+#Option #3 --- Looking up blog and reading it by id
+@app.get("/blogs/{blog_id}", response_model=Blog)
+async def read_blogs(blog_id:UUID):
+    for blog in blogs_db:
+        if blogs_db == blog_id:
+            return blog
+    raise HTTPException(Status_code=404,detail="blog not found")
+#Testing API with HTTP GET via curl command
+#curl -v http://127.0.0.1:8000/get-message?name=Dee
+
+#Option #4 --- Updating blog by id
+@app.put("/blogs/{blog_id}", response_model=Blog)
+async def update_blogs(blog_id: UUID, blog: Blog):
+    for index, stored_blog in enumerate(blogs_db):
+        if stored_blog.id == blog_id:
+            blog_db[index] = blog
+            return blog
+    raise HTTPException(Status_code=404,detail="blog not found")
+#Testing API with HTTP GET via curl command
+#curl -v http://127.0.0.1:8000/get-message?name=Dee
+
+#Option #5 --- Deleting blog by id
+@app.delete("/blogs/{blog_id}", response_model=Blog)
+async def delete_blogs(blog_id: UUID):
+    for index, blog in enumerate(blogs_db):
+        if blog.id == blog_id:
+            return blogs_db.pop(index)
+    raise HTTPException(Status_code=404,detail="blog not found")
+#Testing API with HTTP GET via curl command
+#curl -v http://127.0.0.1:8000/get-message?name=Dee
+
+
+#Reference: https://blog.postman.com/how-to-build-an-api-in-python/
+#Reference: https://arjancodes.com/blog/building-rest-apis-with-python-and-fastapi-tutorial/
+
+def API_Interface():
+    """API interface for user to interact from executing script"""
+    try:
+        print("Welcome to the API interface for blogs")
+        print("...")
+        print("...")
+        print("...")
+        print("Welcome to the main menu! Please select your method of choice")
+        print("[test] ---  Test Connecting to interface!")
+        print("[1] --- Creating blog and adding into array")
+        print("[2] --- Read all blogs within the array")
+        print("[3] --- Looking up blog and reading it by id")
+        print("[4] --- Updating blog by id")
+        print("[5] --- Deleting blog by id")
+        var1=input('Please enter your choice here!')
+        #User input | Allowing sanity test
+        if var1 == "test":
+            test = requests.get('http://127.0.0.1:8000/')
+            print(test.text)
+        #User input | Creating blog and adding into array
+        if var1 == 1:
+            print("WORK IN PROGRESS | [1] --- Creating blog and adding into array")
+        #User input | Reading all blogs within the array
+        if var1 == 2:
+            print("WORK IN PROGRESS | [2] --- Read all blogs within the array")
+        #User input |  Reading a blog by ID within the array
+        if var1 == 3:
+            print("WORK IN PROGRESS | [3] --- Looking up blog and reading it by id")
+        #User input |  Updating a blog by ID within the array
+        if var1 == 4:
+            print("WORK IN PROGRESS | [4] --- Updating blog by id")
+        #User input |  Deleting a blog by ID within the array
+        if var1 == 5:
+            print("WORK IN PROGRESS | [5] --- Deleting blog by id")
+    except:
+        Print("Failture with API processing")
+
+
+try:
+    API_Interface()
+except:
+    print("Unable to reach API interface")
